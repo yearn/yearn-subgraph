7a34ff43ed5532d24b256d3becbefab82ac6c692
thegismar <thegismar@gmail.com>
2020-09-01 14:21:19 -0300

    feat: add ycrv vault, id, address and price

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,25 +1,12 @@
-import {
-  Approval as ApprovalEvent,
-  Transfer as TransferEvent
-} from "../generated/Contract/Contract"
-import { Approval, Transfer } from "../generated/schema"
+import { Address } from "@graphprotocol/graph-ts"
+import {Contract, Transfer} from "../generated/Contract/Contract"
+import { Vault, Deposit, Withdrawal } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
-  entity.save()
-}
+export function handleTransfer(event: Transfer): void {
+  let id = event.address.toHexString()
+  let vault = new Vault(id)
+  let c = Contract.bind(event.address)
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
-  entity.save()
+  vault.price = c.getPricePerFullShare()
+  vault.save()
 }
