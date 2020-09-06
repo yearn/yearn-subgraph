56da4aaf1c03966f8c27a02ddb56871bb6740388
x48 <x48114@protonmail.com>
2020-09-06 11:33:25 -0400

    feat: add deposit/withdraw entities and upgrade vaults/transactions

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,32 +1,84 @@
-import { ethereum } from "@graphprotocol/graph-ts"
-import {V1Contract, Transfer} from "../generated/yUSDVault/V1Contract"
-import {V1CContract} from "../generated/V1Controller/V1CContract"
-import { Vault, Transfer as t } from "../generated/schema"
+import { ethereum } from "@graphprotocol/graph-ts";
+import {
+  V1Contract,
+  DepositCall,
+  WithdrawCall,
+  Transfer as TransferEvent,
+} from "../generated/yUSDVault/V1Contract";
 
-export function handleTransfer(event: Transfer): void {
-  let id = event.address.toHexString()
-  let vault = new Vault(id)
-  let c = V1Contract.bind(event.address)
+import { Address } from "@graphprotocol/graph-ts";
 
-  vault.timestamp = event.block.timestamp
-  vault.block = event.block.number
-  vault.getPricePerFullShare = c.getPricePerFullShare()
-  vault.totalSupply = c.totalSupply()
-  vault.balance = c.balance()
-  vault.available = c.available()
-  vault.token = c.token()
-  vault.symbol = c.symbol()
-  vault.name  = c.name()
-  vault.controller = c.controller()
-  vault.save()
+import { Vault, Deposit, Withdraw, Transfer } from "../generated/schema";
 
-  let id2 = event.transaction.hash.toHexString()
-  let transfer = new t(id2)
-  transfer.vault = vault.id
-  transfer.from = event.params.from
-  transfer.to = event.params.to
-  transfer.amount = event.params.value
-  transfer.timestamp = event.block.timestamp
-  transfer.save()
+function getVault(vaultAddress: Address): Vault {
+  let vault = new Vault(vaultAddress.toHexString());
+  let vaultContract = V1Contract.bind(vaultAddress);
+  vault.balance = vaultContract.balance();
+  vault.getPricePerFullShare = vaultContract.getPricePerFullShare();
+  vault.totalSupply = vaultContract.totalSupply();
+  vault.balance = vaultContract.balance();
+  vault.available = vaultContract.available();
+  vault.token = vaultContract.token();
+  vault.symbol = vaultContract.symbol();
+  vault.name = vaultContract.name();
+  vault.controller = vaultContract.controller();
+  return vault;
+}
 
+export function handleTransfer(event: TransferEvent): void {
+  let vaultAddress = event.address;
+  let vault = getVault(vaultAddress);
+  vault.timestamp = event.block.timestamp;
+  vault.blockNumber = event.block.number;
+  vault.save();
+
+  let transactionAddress = event.transaction.hash.toHexString();
+  let transfer = new Transfer(transactionAddress);
+  transfer.from = event.params.from;
+  transfer.to = event.params.to;
+  transfer.amount = event.params.value;
+  transfer.timestamp = event.block.timestamp;
+  transfer.blockNumber = event.block.number;
+  transfer.vaultAddress = vaultAddress;
+  transfer.getPricePerFullShare = vault.getPricePerFullShare;
+  transfer.balance = vault.balance;
+  transfer.totalSupply = vault.totalSupply;
+  transfer.available = vault.available;
+  transfer.save();
+}
+
+export function handleDeposit(call: DepositCall): void {
+  let transactionAddress = call.transaction.hash.toHexString();
+  let deposit = new Deposit(transactionAddress);
+  let amount = call.inputs._amount;
+  let account = call.from;
+  let vaultAddress = call.to;
+  let vault = getVault(vaultAddress);
+  let totalSupply = vault.totalSupply;
+  let balance = vault.balance;
+  deposit.timestamp = call.block.timestamp;
+  deposit.blockNumber = call.block.number;
+  deposit.account = account;
+  deposit.vaultAddress = vaultAddress;
+  deposit.amount = amount;
+  deposit.shares = (amount * totalSupply) / balance;
+  deposit.save();
+}
+
+export function handleWithdraw(call: WithdrawCall): void {
+  let transactionAddress = call.transaction.hash.toHexString();
+  let withdraw = new Withdraw(transactionAddress);
+  let account = call.from;
+  let shares = call.inputs._shares;
+  let vaultAddress = call.to;
+  let vault = getVault(vaultAddress);
+  let totalSupply = vault.totalSupply;
+  let balance = vault.balance;
+  withdraw.timestamp = call.block.timestamp;
+  withdraw.blockNumber = call.block.number;
+  withdraw.vaultAddress = vaultAddress;
+  withdraw.account = account;
+  withdraw.shares = shares;
+  withdraw.amount = (balance * shares) / totalSupply;
+  withdraw.save();
 }
