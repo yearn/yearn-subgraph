ea8381512a4500a58980a97963702bcaa25f0711
milkyklim <10698619+milkyklim@users.noreply.github.com>
2020-10-24 17:06:51 +0200

    chore: remove Decimal support (broken)

--- src/utils/decimals.ts
+++ src/utils/decimals.ts
@@ -1,28 +1,28 @@
-import { BigDecimal, BigInt } from '@graphprotocol/graph-ts';
+// import { BigDecimal, BigInt } from '@graphprotocol/graph-ts';
 
-export const DEFAULT_DECIMALS = 18;
+// export const DEFAULT_DECIMALS = 18;
 
-export function pow(base: BigDecimal, exponent: number): BigDecimal {
-  let result = base;
+// export function pow(base: BigDecimal, exponent: number): BigDecimal {
+//   let result = base;
 
-  if (exponent == 0) {
-    return BigDecimal.fromString('1');
-  }
+//   if (exponent == 0) {
+//     return BigDecimal.fromString('1');
+//   }
 
-  for (let i = 2; i <= exponent; i++) {
-    result = result.times(base);
-  }
+//   for (let i = 2; i <= exponent; i++) {
+//     result = result.times(base);
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-export function toDecimal(
-  value: BigInt,
-  decimals: number = DEFAULT_DECIMALS,
-): BigDecimal {
-  let precision = BigInt.fromI32(10)
-    .pow(<u8>decimals)
-    .toBigDecimal();
+// export function toDecimal(
+//   value: BigInt,
+//   decimals: number = DEFAULT_DECIMALS,
+// ): BigDecimal {
+//   let precision = BigInt.fromI32(10)
+//     .pow(<u8>decimals)
+//     .toBigDecimal();
 
-  return value.divDecimal(precision);
-}
+//   return value.divDecimal(precision);
+// }
